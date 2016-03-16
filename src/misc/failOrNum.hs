19f5a813da8fb21cba540873303e2985ea6efd4f
Alexander Krasniansky <alexander.krasniansky@macys.com>
2016-03-16 03:49:15 +0200

    'failOrNum' update (Scala comparison)

--- src/misc/failOrNum.hs
+++ src/misc/failOrNum.hs
@@ -4,3 +4,20 @@ fail' = error "Ka-boom!"
 
 failOrNum :: Num b => a -> b -> b
 failOrNum f n = n + 1
+
+{-
+    Here is the same implementation on Scala:
+
+    ```scala
+    object LazyComputation extends App {
+
+      def failOrInt(execution: => Unit, n: Int): Int = n + 1
+
+      println {
+        failOrInt({throw new RuntimeException("Ka-boom!")}, 4)
+      }
+
+    }
+    ```
+-}
+
