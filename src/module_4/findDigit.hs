80b526e2042ff24b75ba08f8a31130302f7d1a49
Alexander Krasniansky <alexander.krasniansky@macys.com>
2016-03-27 17:05:27 +0300

    update module#4 solutions (Haskell vs Scala)

---
+++ src/module_4/findDigit.hs
@@ -0,0 +1,28 @@
+{-
+	Реализуйте функцию, которая ищет в строке первое вхождение символа,
+	который является цифрой, и возвращает Nothing, если в строке нет цифр.
+-}
+
+import Data.Char(isDigit)
+
+findDigit :: [Char] -> Maybe Char
+findDigit xs = helper xs where
+        helper [] = Nothing
+        helper (x:xs)
+            | isDigit x = (Just x)
+            | otherwise = helper xs
+
+{-
+
+	-------------------
+	[The same in Scala]
+	-------------------
+
+	import java.lang.Character.isDigit
+
+	object FindDigit extends App {
+	  def findDigit(s: String): Option[Char] = {
+	    s find {x => isDigit(x)}
+	  }
+	}
+-}
